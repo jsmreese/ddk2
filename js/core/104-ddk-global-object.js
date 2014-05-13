5ea902c4fe97d751448805a9205da230737406cc
John Madhavan-Reese <jsmreese@pureshare.com>
2014-05-13 19:34:48 -0400

    Separating Layout Template code.

--- js/core/104-ddk-global-object.js
+++ js/core/104-ddk-global-object.js
@@ -2154,42 +2154,7 @@
 				fodResize: true,
 				defaultOptions: defaultScorecardOptions2
 			},
-			layout: {
-				options: {
-					paneClass: "pane",
-					resizerClass: "resizer",
-					togglerClass: "toggler",
-					north__closable: false,
-					north__resizable: false,
-					north__spacing_open: 0,
-					center__onsizecontent_start: function () { shrinkLayoutContent(0); },
-					west__onsizecontent_start: function () { shrinkLayoutContent(1); },
-					east__onsizecontent_start: function () { shrinkLayoutContent(2); },
-					south__onsizecontent_start: function () { shrinkLayoutContent(3); },
-					center__onresize_end: function () { fnResizeLayoutContent(0); },
-					west__onresize_end: function () { fnResizeLayoutContent(1); },
-					east__onresize_end: function () { fnResizeLayoutContent(2); },
-					south__onresize_end: function () { fnResizeLayoutContent(3); }
-				},
-				options2: {
-					paneClass: "outerpane",
-					//resizerClass: "resizer",
-					//togglerClass: "toggler",
-					north__closable: false,
-					north__resizable: false,
-					north__spacing_open: 0,
-					center__paneSelector: ".ui-layout-outer-center",
-					center__children: {
-						paneClass: "pane",
-						resizerClass: "resizer",
-						togglerClass: "toggler"
-					}
-				},
-				center: {},
-				west: {},
-				east: {},
-				south: {}
-			},
+
 			control: {
 				init: initControl
 			},
@@ -2512,42 +2477,7 @@
 		});
 	};
 	
-DDK.spoofMediaQuery = function () {
-	var $center = $("#layout_content_center"),
-		width = $center.width(),
-		sizes = DDK.spoofMediaQuery.sizes,
-		ranges = DDK.spoofMediaQuery.ranges,
-		classList = _.map(sizes, function (size) {
-			return "ddk-mq-" + size;
-		});
 
-	$center.removeClass(classList.join(" "));
-		
-	_.each(sizes, function (size, index) {
-		if (width > ranges[size].min && width <= ranges[size].max) {
-			$center.addClass(classList[index]);
-		}
-	});
-	
-	// resize controls in the center pane that are contained in grid row elements
-	$center.find(".row").findControls().resizeControls();
-};
-
-DDK.spoofMediaQuery.sizes = "small medium large".split(" ");
-DDK.spoofMediaQuery.ranges = {
-	small: {
-		min: 0,
-		max: 480
-	},
-	medium: {
-		min: 480,
-		max: 1024
-	},
-	large: {
-		min: 1024,
-		max: Infinity
-	}
-};
 
 DDK.chart.updateImage = function (controlId) {
 	var controlName = "chart",
