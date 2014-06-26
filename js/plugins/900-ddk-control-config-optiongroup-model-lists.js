0311e60301edde1c466588315cab8b7d406be3a9
John Madhavan-Reese <jsmreese@pureshare.com>
2014-06-26 13:45:29 -0400

    Added check for OptionGroup.setup before executing setup.

--- js/plugins/900-ddk-control-config-optiongroup-model-lists.js
+++ js/plugins/900-ddk-control-config-optiongroup-model-lists.js
@@ -1,21 +1,27 @@
-DDK.bamset2.configOptionGroupModels = [
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.bamset),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.bam),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.formatElement)
-];
+// DDK 2.2.0b2
+// wrapping in a check for OptionGroup.setup
+// to avoid breaking the Metric Input forms, which use a custom
+// implementation of PS.MC.Models.OptionGroup
+if (PS.MC.Models.OptionGroup && PS.MC.Models.OptionGroup.setup) {
+	DDK.bamset2.configOptionGroupModels = [
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.bamset),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.bam),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.formatElement)
+	];
 
-DDK.scorecard2.configOptionGroupModels = [
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.scorecard),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.scorecardColumn)
-];
+	DDK.scorecard2.configOptionGroupModels = [
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.scorecard),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.scorecardColumn)
+	];
 
-DDK.navset2.configOptionGroupModels = [
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navset),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.nav),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navElem),
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navElemConfig)
-];
+	DDK.navset2.configOptionGroupModels = [
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navset),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.nav),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navElem),
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.navElemConfig)
+	];
 
-DDK.list.configOptionGroupModels = [
-	(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.list)
-];
+	DDK.list.configOptionGroupModels = [
+		(new PS.MC.Models.OptionGroup()).setup(PS.optionsAPI.list)
+	];
+}
