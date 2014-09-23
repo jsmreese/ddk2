5b3580fcd1fd2c2412a054d6dbf360bff7a71a97
John Madhavan-Reese <jsmreese@pureshare.com>
2014-09-23 12:40:58 -0400

    Do not close off-canvas menu when clicking on a select2.

--- js/plugins/548-ddk-event-handler-R.js
+++ js/plugins/548-ddk-event-handler-R.js
@@ -1,2 +1,7 @@
 // attach window resize handler
 $(window).on("resize", _.debounce(function () { $(document).findControls().resizeControls(); }, 250));
+
+// do not close off-canvas menu when clicking on a select2 input
+$(".left-off-canvas-menu").on("click", ".select2-choice", function (e) { 
+	e.stopPropagation();
+});
