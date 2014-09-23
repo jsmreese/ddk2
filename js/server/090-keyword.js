ec3203d2c3369ab98546968afcb6e5df1c6e5bac
John Madhavan-Reese <jsmreese@pureshare.com>
2014-09-23 10:45:58 -0400

    Added K.scope method.

--- js/server/090-keyword.js
+++ js/server/090-keyword.js
@@ -474,5 +474,43 @@ _.extend(K, {
 	
 	isKeyword: function(key) {
 		return _.string.toBoolean(containsKeyword(key));
+	},
+	
+	scope: function (func, keywords) {
+		var hasKeywords,
+			result,
+			cache = {};
+		
+		if (typeof keywords === "string" && _.string.isQueryString(keywords)) {
+			keywords = _.string.parseQueryString(keywords);
+		}
+		
+		if (_.isPlainObject(keywords)) {
+			hasKeywords = true;
+			
+			// for each keyword, cache the global state
+			// and update the keyword value
+			_.each(keywords, function (value, key) {
+				key = _.string.underscored(key);
+				cache[key] = (K.isKeyword(key) ? value : null);
+				K(key, evalKeywordValue(value));
+			});
+		}
+		
+		result = func();
+		
+		if (hasKeywords) {
+			// restore keyword state
+			_.each(cache, function (value, key) {
+				if (value === null) {
+					KeywordDelete(key);
+					return;
+				}
+				
+				K(key, value);
+			});
+		}
+		
+		return result;
 	}
 });
