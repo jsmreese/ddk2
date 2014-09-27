0188b0e43d2e341e92bc68f0dde04eb64c140599
John Madhavan-Reese <jsmreese@pureshare.com>
2014-09-27 01:25:13 -0400

    Responsive event handlers.

--- js/plugins/548-ddk-event-handler-R.js
+++ js/plugins/548-ddk-event-handler-R.js
@@ -1,17 +1,19 @@
 // attach window resize handler
 $(window).on("resize", _.debounce(function () { $(document).findControls().resizeControls(); }, 250));
 
-// do not close off-canvas menu when clicking on a select2 input
-$(".left-off-canvas-menu").on("click", ".select2-choice", function (e) { 
-	e.stopPropagation();
-});
+$(document).on("contentloaded", function () {
+	// do not close off-canvas menu when clicking on a select2 input
+	$(".left-off-canvas-menu").on("click", ".select2-choice", function (e) { 
+		e.stopPropagation();
+	});
 
-// section expand/collapse
-$(".left-off-canvas-menu").on("click", ".section-header", function (e) {
-	var $this;
-	
-	$this = $(this);
-	
-	$this.toggleClass("open closed");
-	$this.next(".section-content").slideToggle(DDK.ease);
+	// section expand/collapse
+	$(".left-off-canvas-menu").on("click", ".section-header", function (e) {
+		var $this;
+		
+		$this = $(this);
+		
+		$this.toggleClass("open closed");
+		$this.next(".section-content").slideToggle(DDK.ease);
+	});
 });
