61c4c7b826c23b12ec9e4d01500da3634adf343e
John Madhavan-Reese <jsmreese@pureshare.com>
2014-10-29 16:01:37 -0400

    Added new docs folder.

---
+++ js/docs/001-javascript-client.js
@@ -0,0 +1,22 @@
+PS.optionsAPI.clientJavaScript = {
+	"id": "client_javascript",
+	"label": "Client JavaScript",
+	"description": "Functions included in the DDK Client JavaScript libraries.",
+
+	"section": {
+		"id": "section",
+		"label": "Dummy Section",
+		"description": "Description for dummy section.",
+		"options": {
+			"attr": {
+				"id": "elem_attr",
+				"label": "Attributes",
+				"description": "Element attributes.",
+				"notes": "Sets attributes on the element div.",
+				"version": "",
+				"deprecated": false
+			}
+
+		}
+	}                
+};
