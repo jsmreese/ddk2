9a48f12b04db7df0670d02dd2583e8efeddfa3a4
John Madhavan-Reese <jsmreese@pureshare.com>
2014-10-30 16:07:17 -0400

    Client JS function list.

--- js/docs/001-javascript-client.js
+++ js/docs/001-javascript-client.js
@@ -20,3 +20,265 @@ PS.optionsAPI.clientJavaScript = {
 		}
 	}                
 };
+
+
+Modernizr v2.7.2
+jQuery JavaScript Library v2.1.1
+jQuery Migrate v1.2.1
+jQuery UI - v1.10.3
+
+jQuery UI fixes:
+	$.ui.dialog.prototype._allowInteraction
+	$.ui.tabs.prototype._setupHeightStyle
+	$.ui.tabs.prototype._getList
+
+jQuery Function Wrappers
+	$.fn.empty
+
+jQuery Plugins:
+	$.target
+	$.topZ
+	$.loadScript
+	$.createStylesheet
+
+	$.fn.isControl
+	$.fn.reloadControls
+	$.fn.reload // deprecated
+	$.fn.reloadControlsQueue
+	$.fn.resizeControls
+	$.fn.initControls
+	$.fn.findControls
+	$.fn.parentControl
+	$.fn.controlData
+
+	$.fn.dataStack
+	$.fn.editor
+	$.fn.editor.defaults
+	$.fn.rowspan
+	$.fn.reverse
+	$.fn.totalHeight
+	$.fn.scaleAreas
+	$.fn.maxWidth
+	$.fn.maxHeight
+	$.fn.convertToHtmlTable
+	$.fn.appendEachCol
+	$.fn.breakTableByWidth
+	$.fn.breakTableByWidth.defaults
+	$.fn.breakTableByHeight
+	$.fn.breakTableByHeight.defaults
+	$.fn.expandControlTableParents
+
+http://mths.be/placeholder v2.0.7 by @mathias
+jQuery BBQ: Back Button & Query Library - v1.2.1
+jQuery hashchange event - v1.2
+
+Lo-Dash 2.4.1
+
+Lo-Dash Mixins:
+	_.delegator
+	_.collate
+	_.guid
+	_.prune
+	_.isRealNumber
+	_.isPositiveInteger
+	_.zipNestedObject
+	_.isPairsArray
+	_.hasData
+	_.hasNumericData
+	_.toRecordObjects
+	_.toRecordArrays
+	_.createCaseConverter
+	_.toUpperCase
+	_.toLowerCase
+	_.isWidgetName
+	_.isModuleName
+	_.isTemplateName
+	_.toCase
+	_.collateTree
+	_.overlay
+	_.overlayValue
+	_.favTreeToOptionGroup
+
+Lo-Dash String (_.str or _.string)
+	_.str.isBlank
+	_.str.stripTags
+	_.str.capitalize
+	_.str.chop
+	_.str.clean
+	_.str.count
+	_.str.chars
+	_.str.swapCase
+	_.str.escapeHTML
+	_.str.unescapeHTML
+	_.str.escapeRegExp
+	_.str.splice
+	_.str.insert
+	_.str.contains
+	_.str.join
+	_.str.lines
+	_.str.reverse
+	_.str.startsWith
+	_.str.endsWith
+	_.str.succ
+	_.str.titleize
+	_.str.camelize
+	_.str.underscored
+	_.str.dasherize
+	_.str.classify
+	_.str.humanize
+	_.str.trim
+	_.str.ltrim
+	_.str.rtrim
+	_.str.truncate
+	_.str.prune
+	_.str.words
+	_.str.pad
+	_.str.lpad
+	_.str.rpad
+	_.str.lrpad
+	_.str.toNumber
+	_.str.numberFormat
+	_.str.strRight
+	_.str.strRightBack
+	_.str.strLeft
+	_.str.strLeftBack
+	_.str.toSentence
+	_.str.slugify
+	_.str.surround
+	_.str.quote
+	_.str.repeat
+	_.str.levenshtein
+	_.str.nameify
+	_.str.toBoolean
+	_.str.isQueryString
+	_.str.parseJSON
+	_.str.parseQueryString
+	_.str.parse
+	_.str.parseTaggedList
+	_.str.coerce
+	_.str.coerce.coerceType
+	_.str.coerce.coerceTriggers
+
+
+Backbone.js 1.1.0
+
+Backbone Fixes:
+	this.options
+
+Backbone.Epoxy
+
+Backbone and Backbone Epoxy Config:
+	Backbone.Epoxy.binding.config({ optionValue: "id" });
+	Backbone.emulateHTTP = true;
+
+moment.js version : 2.3.1
+
+Moment fixes:
+	moment.fn.humanize = _.partial(moment.fn.fromNow, true);
+	moment.duration.fn.fromNow = _.partial(moment.duration.fn.humanize, 
+
+Moment Duration Format v1.2.1
+numeral.js version : 1.5.1
+Ratio.js version 0.4.0
+
+AMEngine AJAX API
+	//GlobalVars  (Dashboard Ajax Api)
+	daa_showloading=false;
+	daa_polling=50;
+	daa_urlitem="";  //append to URL
+	daa_cursor="";
+	daa_showerror=true;
+	daa_returnfunction;
+	daa_showmask;
+	daa_detectlogin=true;
+	daa_serialize=true;
+	daa_showmask = true;
+	
+	util.shouldDebug
+	util.membersEqual
+	util.describe
+	util.debug
+	util.error
+	util.trim
+	util.strip
+	
+	$_
+	$C
+	Try.these
+	
+	getElementsByClassName
+	extractIFrameBody
+	
+	DELAY
+	steps
+	andThen
+	log
+	createXMLHttpRequest
+	
+	ajaxCaller
+	
+	daaHash
+	daaURLGet
+	daaURLFlush
+	daaGetValue
+	daaURLUpdate
+	daaHashUpdate
+	daaHashGet
+	daaHashList
+	daaHashSet
+	
+	run
+	load
+	
+	reloadMetricNameInto
+	reloadMetricInto
+	reloadMetricByName
+	reloadMetric
+	reloadAll
+	GetDash
+	drawEachMetric
+	runDash
+	
+	$.fn.serializeAnything
+	
+	ProcessURL3
+	
+	hideMask
+	getInlineStyle
+	showMask
+	
+	ProcessURL2
+	
+	delay
+	
+	ProcessURL
+	
+	getQueryString
+	
+	getError
+	getErrorHtml
+	isError
+	
+	isLoginPage
+	checkLoginPage
+	
+	returnContents
+	processReqChange2
+	
+	$.am
+	$.fn.am
+	
+	Hashtable
+	
+	URLEncode
+	URLDecode
+	
+	getQueryString
+	parseQueryString
+
+	
+Next is 
+/*******************************************
+ * File: js/core/041-keyword.js
+ */
+	
