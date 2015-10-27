d058de284f8af75bbee7e53c6d9816f118d59ebe
jacksontan <jtan@pureshare.com>
2015-10-28 00:12:58 +0800

    removed overlib

--- js/core/080-overlib.js
+++
@@ -1,9 +0,0 @@
-// define overlib() and nd() so that calls to these functions will produce
-// friendly DDK errors rather than raw JavaScript errors
-function overlib() {
-	DDK.error("overlib is not enabled. To enable overlib, use the DDK Configuration Dashboard: amengine.aspx?config.mn=DDK2_Config");
-}
-
-function nd() {
-	DDK.error("overlib is not enabled. To enable overlib, use the DDK Configuration Dashboard: amengine.aspx?config.mn=DDK2_Config");
-}
